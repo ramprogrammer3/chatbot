96c8b44876ad6035ab715967f1f580589b12bff3
ram kumar <rammaniyari@gmail.com>
2023-11-17 21:08:07 +0530

    login form

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,38 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-  return (
-    <div>
-      <div>
-        
-        <p>chatbot</p>
 
-        <div>
+  const {user} = useSelector((state)=>state.user);
+  console.log(user);
+  return (
+    <div className='py-2 border-b-2'>
+      <div className='w-11/12 max-w-[1080px] mx-auto flex justify-between items-center'>
+
+        <Link to={"/"} className='text-2xl tracking-wider'>chatbot</Link>
+
+        <div className='flex gap-x-4'>
+
+          {
+            !user && <Link to={"/login"}>Login</Link>
+          }
+
+          {
+            !user && <Link to={"/signup"}>Signup</Link>
+          }
+
+          {
+            user && <Link to={"/chat"}> Go to chat </Link>
+          }
+
+          {
+            user && <button>Logout</button>
+          }
 
         </div>
 
       </div>
-      <h1>navbar component </h1>
     </div>
   )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,76 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
+  const [formdata, setFormdata] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormdata((prevData) => {
+      return {
+        ...prevData,
+        [e.target.name]: e.target.value,
+      };
+    });
+  };
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    console.log(formdata)
+  }
+
   return (
     <div>
-      <h1>Login page </h1>
-    </div>
-  )
-}
+      <div className="w-11/12 max-w-[500px] mx-auto  mt-14 border border-blue-300 p-10 rounded-md">
+        <p className="text-center text-2xl tracking-wider">login page </p>
+        <form className="mt-5 flex flex-col gap-y-3" onSubmit={handleSubmit}>
+          <label>
+            <p>Email : </p>
+            <input
+              type="email"
+              name="email"
+              onChange={handleChange}
+              value={formdata.email}
+              placeholder="Enter email "
+              className="w-full border border-blue-300
+             focus:outline-blue-400 rounded-md text-center"
+              required
+            />
+          </label>
 
-export default Login
+          <label>
+            <p>Password : </p>
+            <input
+              type="password"
+              name="password"
+              onChange={handleChange}
+              value={formdata.password}
+              placeholder="Enter password "
+              className="w-full border border-blue-300
+             focus:outline-blue-400 rounded-md text-center"
+              required
+            />
+          </label>
+          <button
+            type="submit"
+            className="bg-blue-500 text-white rounded-md 
+            cursor-pointer hover:bg-blue-600 transition-all duration-200"
+          >
+            Login
+          </button>
+        </form>
+        <div className="text-center mt-4">
+          <span className="text-red-500">Not account ? </span>
+          <Link to={"/signup"} className="text-blue-600">
+            {" "}
+            signup{" "}
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
