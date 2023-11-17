4a90cf2ce5bd4402f96ad0dcca8703e8aa7f0fe8
ram kumar <rammaniyari@gmail.com>
2023-11-18 00:22:59 +0530

    singup controller

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,5 +1,6 @@
 
 const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
 
 exports.login = async(req,res)=>{
     try {
@@ -14,7 +15,46 @@ exports.login = async(req,res)=>{
 
 exports.signup = async(req,res)=>{
     try {
-        
+
+        // fetch data from request body
+        const {name,email,password} = req.body;
+        // validate data
+        if(!name || !email || !password){
+            return res.status(400).json({
+                success : false,
+                message : "All fields are required"
+            })
+        }
+
+        // check if user is already register
+        const existUser = await User.findOne({email});
+
+        if(existUser){
+            return res.status(400).json({
+                success : false,
+                message : "User is already register"
+            })
+        }
+
+        // assing user image using dice bearer
+        let image = `https://api.dicebear.com/7.x/pixel-art/svg?seed=${name}`;
+
+
+        // hash password
+        const hashPassword = await bcrypt.hash(password,10)
+
+        // register user
+        const user = await User.create({
+            name,email,passowrd : hashPassword,
+            image
+        })
+        // sending response to user
+
+        return res.status(201).json({
+            success : true,
+            message : "User register successfully",
+            user
+        }) 
     } catch (error) {
         return res.status(500).json({
             success :false,
